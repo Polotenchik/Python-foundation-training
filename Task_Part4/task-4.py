7043e30f9ffb644be484265300a311d2bb0d32d1
Nikita Hapeyeu <Mikita_Hapeyeu@epam.com>
2019-05-03 16:12:16 +0300

    refactor: add naming to args and kwargs

--- Task_Part4/task-4.py
+++ Task_Part4/task-4.py
@@ -1,17 +1,27 @@
 import datetime as dt
-import functools
+import functools 
+import itertools
+import inspect
 
 def log(func):
     @functools.wraps(func)
     def wrap(*args, **kwargs):
-        #print("{}: Starting {}.".format(dt.datetime.now(), func.__name__))
-        print("Arguments were: {} and {}.".format(args, kwargs))
-        #start = dt.datetime.now()
-        #res = func(*args, **kwargs)
-        #end = dt.datetime.now()
-        #running_time = (end - start).seconds
-        #print("{}: I'm done. Took {} seconds to finish.".format(dt.datetime.now(), running_time))
-        #return res
+
+        inp_args = inspect.getfullargspec(func)
+        params = inp_args[0]
+        params_dict = dict(enumerate(params))
+        list = []
+        for p_key in params_dict:
+            list.append(params_dict[p_key])
+        lenList = list[:len(args)]
+
+        output = ''
+        output += func.__name__ + ';'
+        if args:
+            output += ' args: '+ ', '.join('%s=%r' % (lenList[i], args[i]) for i in range(0, len(lenList))) + ';'
+        if kwargs:
+            output += ' kwargs: ' + ', '.join('%s=%r' % (key, value) for key, value in kwargs.items()) + ';'
+        print(output)
 
     return wrap
 
@@ -20,6 +30,13 @@ def log(func):
 def foo(a, b, c):
     return a + b + c
 
+@log
+def bar(a, b, c, d):
+    return a + b + c - d
+
 foo(1, 2, c=3)
+foo(1, 2)
+bar(1, 2, c = 3, d = 4)
+
 
 input()
