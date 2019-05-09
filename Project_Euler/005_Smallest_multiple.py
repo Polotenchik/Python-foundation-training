abd8bffba1c5f9b15d1a4d98ab590429df99a85e
Polotenchik Koala <Polotenchik.koala@gmail.com>
2019-05-09 12:43:31 +0300

    solve problem-5

---
+++ Project_Euler/005_Smallest_multiple.py
@@ -0,0 +1,24 @@
+'''2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?'''
+
+def gcd(a,b):
+    """Compute the greatest common divisor of a and b"""
+    while b > 0:
+        a, b = b, a % b
+    return a
+    
+def lcm(a, b):
+    """Compute the lowest common multiple of a and b"""
+    return a * b / gcd(a, b)
+
+result = 1
+i = 2
+n = 20
+
+while i <= n:
+    result = lcm(result,i)
+    i = i + 1
+
+print(result) # 232792560
+
+input()
